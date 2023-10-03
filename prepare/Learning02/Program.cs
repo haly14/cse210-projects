25bc07b5c10eeae18e1a984e5680dd9647b3f46a
haly14 <haly14@gmail.com>
2023-10-02 20:32:57 -0600

    job part of main program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Junior Software Developer";
+        job1._company = "Microsoft";
+        job1._startYear = 2015;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._jobTitle = "AI Software Developer";
+        job2._company = "Google";
+        job2._startYear = 2018;
+        job2._endYear = 2023;
+
+
     }
 }
