0dad757b4888c165049ffe26ab93272b56c99ea2
haly14 <haly14@gmail.com>
2023-10-02 20:49:35 -0600

    start resume.cs. edits to job, program, resume.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,10 +7,21 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Junior Software Developer";
         job1._company = "Microsoft";
+        job1._startYear = 2015;
+        job1._endYear = 2018;
 
         Job job2 = new Job();
         job2._jobTitle = "AI Software Developer";
         job2._company = "Google";
+        job2._startYear = 2018;
+        job2._endYear = 2023;
 
+        Resume myResume = new Resume();
+        myResume._name = "Haley Wellard Atkinson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
