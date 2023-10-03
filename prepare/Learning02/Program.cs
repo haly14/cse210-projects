6960e5a11410ceaeda011589c61949665ec6cc38
haly14 <haly14@gmail.com>
2023-10-02 20:35:08 -0600

    edits to job portion of program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,15 +7,10 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Junior Software Developer";
         job1._company = "Microsoft";
-        job1._startYear = 2015;
-        job1._endYear = 2018;
 
         Job job2 = new Job();
         job2._jobTitle = "AI Software Developer";
         job2._company = "Google";
-        job2._startYear = 2018;
-        job2._endYear = 2023;
-
 
     }
 }
