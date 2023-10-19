69dd3982958c7fc64ce8fee714e3e4eabb8f55ac
haly14 <haly14@gmail.com>
2023-10-18 21:14:33 -0600

    dev03 simple framework

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("Philipians 4:13 I can do all things through Christ which strengtheneth me.");
+        Console.WriteLine("Press enter to continue to type 'quit' to finish.");
+        Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine("Philipians 4:13 I can do ___ ______ through Christ which strengtheneth me.");
+        Console.WriteLine("Press enter to continue to type 'quit' to finish.");
+        Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine("Philipians 4:13 I can do ___ ______ through ______ which _____________ me.");
+        Console.WriteLine("Press enter to continue to type 'quit' to finish.");
+        Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine("Philipians 4:13 _ ___ do ___ ______ through ______ which _____________ __.");
+        Console.WriteLine("Press enter to continue to type 'quit' to finish.");
+        Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine("Philipians 4:13 _ ___ __ ___ ______ _______ ______ _____ _____________ __.");
+        Console.WriteLine("Now try to recite the entire scripture from memory!");
     }
 }
