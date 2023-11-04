fcebd97b8ba3afeed6c4aac42be69948ea4a32ea
haly14 <haly14@gmail.com>
2023-11-04 09:25:05 -0600

    edits to syntax, capitalization. fix word hiding

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,12 +1,11 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Create a scripture instance
         var scripture = new Scripture("Philippians 4:13", "I can do all things through Christ which strengtheneth me.");
-
         Console.WriteLine(scripture);
 
         while (scripture.Text.Contains("_"))
@@ -19,7 +18,7 @@ class Program
                 break;
             }
 
-            scripture.HideWords(2); // Replace 2 words with underscores
+            scripture.HideWords(3); // Hide 3 random words
             Console.Clear();
             Console.WriteLine(scripture);
         }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,6 +1,7 @@
 public class Scripture
 {
     private string _text;
+    private List<string> _words;
 
     public Reference Reference { get; private set; }
     public string Text
@@ -13,16 +14,46 @@ public class Scripture
     {
         Reference = new Reference(fullReference);
         Text = text;
+        _words = new List<string>(text.Split(' '));
     }
 
     public void HideWords(int count)
     {
-        // Implement logic to hide 'count' number of words in the scripture text
+        for (int i = 0; i < count; i++)
+        {
+            int index = GetRandomHiddenWordIndex();
+            if (index >= 0)
+            {
+                _words[index] = new string('_', _words[index].Length);
+            }
+        }
+
+        Text = string.Join(" ", _words);
+    }
+
+    private int GetRandomHiddenWordIndex()
+    {
+        var hiddenWordsIndices = new List<int>();
+        for (int i = 0; i < _words.Count; i++)
+        {
+            if (_words[i].Contains("_"))
+            {
+                hiddenWordsIndices.Add(i);
+            }
+        }
+
+        if (hiddenWordsIndices.Count > 0)
+        {
+            Random random = new Random();
+            int randomIndex = random.Next(hiddenWordsIndices.Count);
+            return hiddenWordsIndices[randomIndex];
+        }
+
+        return -1;
     }
 
     public override string ToString()
     {
-        // Implement ToString() to display the scripture with reference
         return $"{Reference.FullReference} - {Text}";
     }
 }
