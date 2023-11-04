075df95288e1a8cce2021ac83ed4857f49a05fa4
haly14 <haly14@gmail.com>
2023-11-04 09:47:44 -0600

    add to user experience, instruction message

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,21 +7,29 @@ class Program
         var scripture = new Scripture("Philippians 4:13", "I can do all things through Christ which strengtheneth me.");
         Console.WriteLine(scripture);
 
-        while (scripture.Text.Contains("_"))
+        int wordsToHide = 3; // Number of words to hide
+        bool quit = false;
+
+        while (scripture.Text.Contains("_") && !quit)
         {
             Console.WriteLine("Press enter to continue or type 'quit' to finish.");
             string userInput = Console.ReadLine();
 
             if (userInput == "quit")
             {
-                break;
+                quit = true;
             }
-
-            scripture.HideWords(3); // Hide 3 random words
-            Console.Clear();
-            Console.WriteLine(scripture);
+            else
+            {
+                scripture.HideWords(wordsToHide);
+                Console.Clear();
+                Console.WriteLine(scripture);
+            }
         }
 
-        Console.WriteLine("Now try to recite the entire scripture from memory!");
+        if (!quit)
+        {
+            Console.WriteLine("Now try to recite the entire scripture from memory!");
+        }
     }
 }
