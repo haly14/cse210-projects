fd8b456f70f019c33d445c1a18c773ed6aeda756
haly14 <haly14@gmail.com>
2023-11-04 11:59:58 -0600

    Create classes for dev4

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        while (true)
+                {
+                    Console.WriteLine("Menu Options:");
+                    Console.WriteLine("1. Start breathing activity");
+                    Console.WriteLine("2. Start reflecting activity");
+                    Console.WriteLine("3. Start listing activity");
+                    Console.WriteLine("4. Quit");
+                    Console.WriteLine("Select an option from the menu above.");
+                    
+                }
     }
 }
