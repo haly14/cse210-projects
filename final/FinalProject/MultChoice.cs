4e221d22666de68060b4448c55d0f3a9abe8113f
haly14 <haly14@gmail.com>
2023-12-11 17:54:25 -0700

    multchoice question generator

--- final/FinalProject/MultChoice.cs
+++ final/FinalProject/MultChoice.cs
@@ -3,7 +3,40 @@ public class MultChoice : Application
 {
     public MultChoice(string answer, int points) : base(answer, points)
     {
-        Console.Write("Multiple Choice Question 2: ");
         OriginalPoints = points;
+    }
+
+    public void GenerateRandomQuestion(int questionNumber)
+    {
+        switch (questionNumber)
+        {
+            case 1:
+                Console.Write("Multiple Choice Question 1: ");
+                break;
+            case 2:
+                Console.Write("Multiple Choice Question 2: ");
+                break;
+            case 3:
+                Console.Write("Multiple Choice Question 3: ");
+                break;
+            case 4:
+                Console.Write("Multiple Choice Question 4: ");
+                break;
+            case 5:
+                Console.Write("Multiple Choice Question 5: ");
+                break;
+            case 6:
+                Console.Write("Multiple Choice Question 6: ");
+                break;
+            case 7:
+                Console.Write("Multiple Choice Question 7: ");
+                break;
+            case 8:
+                Console.Write("Multiple Choice Question 8: ");
+                break;
+            default:
+                Console.WriteLine("Invalid question number.");
+                break;
+        }
     }
 }
